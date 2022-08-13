c3dd7fe919f21acd1710fff020700b983e06ec8a
doniyor <doniyor@example.com>
2022-08-13 19:45:06 +0500

    first commit

---
+++ QRwpf/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using QRwpf.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QRwpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ScanQRBtn_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGeneratorService codeGeneratorService = new();
+            QRCodeImg.Source =  new BitmapImage( new Uri(codeGeneratorService.GenerateQr(QRText.Text)));
+            //QRCodeImg.Margin = new Thickness(270, 0, 0, 300);
+
+
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ QRwpf/Service/QRCodeGeneratorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+namespace QRwpf.Service
+{
+    public class QRCodeGeneratorService
+    {
+        private QRCodeEncoder encoder = new QRCodeEncoder();
+        Bitmap bitmap ;
+        public const string BASE_URL = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=";
+
+        string filepath;
+        public string GenerateQr(string data)
+        {
+            encoder.QRCodeScale = 8;
+            bitmap = encoder.Encode(data);
+            ToImage(bitmap);
+            return filepath;
+        }
+        public void ToImage(Bitmap bitmap)
+        {
+                filepath = $"C:/Users/asus/source/repos/QRwpf/QRwpf/Images/{Guid.NewGuid():N}.png";
+                bitmap.Save(filepath, ImageFormat.Png);
+        }
+    }
+}
